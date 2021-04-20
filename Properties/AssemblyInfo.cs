83a0ac2604ff9c59dfb075209f8f03f5547a7680
Brian Brice <bbrice@gmail.com>
2021-04-20 12:15:25 -0500

    reword license preamble

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,23 @@
-﻿using System.Reflection;
+﻿/*
+ * Copyright (C) 2021, Brian Brice. All rights reserved.
+ *
+ * This file is part of svn-custom-depth.
+ *
+ * svn-custom-depth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * svn-custom-depth is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with svn-custom-depth.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
